23c432284cfb0e84867847ee11950935a0f1da36
Yubin Jamora <yubinjamora@Yubins-MacBook-Pro.local>
2017-11-21 20:23:10 -0500

    new repo

---
+++ data/friends.js
@@ -0,0 +1,103 @@
+
+var friends = [
+    {
+      name: "Korgi",
+      photo: "https://i.pinimg.com/736x/c8/e7/e6/c8e7e6bed6fc459ff62ac1f11abf7987--dog-stroller-adorable-puppies.jpg",
+      scores: [
+        "2",
+        "1",
+        "3",
+        "4",
+        "2",
+        "1",
+        "5",
+        "2",
+        "3",
+        "1"
+      ]
+    },
+    {
+      name: "Poodle",
+      photo: "https://data.whicdn.com/images/48419708/large.jpg",
+      scores: [
+        "4",
+        "1",
+        "4",
+        "4",
+        "2",
+        "1",
+        "2",
+        "3",
+        "3",
+        "1"
+      ]
+    },
+    {
+      name: "Lab",
+      photo: "https://www.thelabradorsite.com/wp-content/uploads/2011/12/black-labs-puppies.jpg",
+      scores: [
+        "5",
+        "3",
+        "4",
+        "1",
+        "4",
+        "5",
+        "3",
+        "2",
+        "2",
+        "5"
+      ]
+    },
+    {
+      name: "Pug",
+      photo: "https://i.ytimg.com/vi/wRx3Uvcktm8/maxresdefault.jpg",
+      scores: [
+        "4",
+        "3",
+        "2",
+        "1",
+        "5",
+        "2",
+        "4",
+        "2",
+        "1",
+        "1"
+      ]
+    },
+    {
+      name: "French Bulldog",
+      photo: "https://i.pinimg.com/736x/c7/88/61/c788617dd1ea186cb78c52248e6a56b8--pics-of-animals-exotic-animals.jpg",
+      scores: [
+        "4",
+        "2",
+        "4",
+        "2",
+        "3",
+        "2",
+        "4",
+        "1",
+        "1",
+        "3"
+      ]
+    },
+    {
+      name: "Shiba Inu",
+      photo: "https://www.animalssale.com/uploads/1500x1500/14490af2815536f10f05417a84e242765467d.jpg?v6",
+      scores: [
+        "4",
+        "4",
+        "1",
+        "3",
+        "2",
+        "2",
+        "2",
+        "2",
+        "3",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  
+  module.exports = friends;
